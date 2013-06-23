ea1b804a6869057344c60b781eb8d2d8d5e03987
Matt Campbell <matt@mattcampbell.net>
2013-06-23 19:45:19 -0400

    Grunt setup

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.initConfig({
+		coffee: {
+			compile: {
+				files: [{
+					expand: true,
+					cwd: 'src/',
+					src: ['**/*.coffee'],
+					dest: 'build',
+					ext: '.js'
+				}]
+			}
+		},
+		watch: {
+			scripts: {
+				files: ['src/**/*.coffee'],
+				tasks: ['coffee:compile'],
+				options: {
+				}
+			}
+		}
+    });
+
+    grunt.registerTask('default', ['coffee:compile']);
+}
