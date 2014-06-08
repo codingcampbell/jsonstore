71b0d8ff772a35214d067c1bb38c2f04b26239e3
Matt Campbell <matt@mattcampbell.net>
2014-06-08 08:51:48 -0400

    Add `grunt publish` to make NPM-compatible tarball
    Outputs tarball to `release` folder,
    which can be installed in NPM by:
    
    `npm install /path/to/release/jsonstore-0.0.0.tar.gz`

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,25 @@
 module.exports = function(grunt) {
-    grunt.loadNpmTasks('grunt-contrib-coffee');
-	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-coffee');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-cafe-mocha');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-    grunt.initConfig({
+	grunt.initConfig({
+		cafemocha: {
+			all: {
+				src: 'test/**/*.js',
+				options: {
+					ui: 'bdd',
+					reporter: 'spec',
+					require: 'should'
+				}
+			}
+		},
+		clean: {
+			build: ['build/']
+		},
 		coffee: {
 			compile: {
 				files: [{
@@ -24,6 +40,25 @@ module.exports = function(grunt) {
 				}]
 			}
 		},
+		compress: {
+			build: {
+				options: {
+					archive: 'release/<%= pkg.name %>-<%= pkg.version %>.tar.gz',
+					mode: 'tgz'
+				},
+				expand: true,
+				cwd: 'build/',
+				src: ['**/*'],
+				dest: '<%= pkg.name %>-<%= pkg.version %>/'
+			}
+		},
+		copy: {
+			build: {
+				src: 'package.json',
+				dest: 'build/'
+			}
+		},
+		pkg: grunt.file.readJSON('package.json'),
 		watch: {
 			scripts: {
 				files: ['src/**/*.coffee'],
@@ -31,19 +66,16 @@ module.exports = function(grunt) {
 				options: {
 				}
 			}
-		},
-		cafemocha: {
-			all: {
-				src: 'test/**/*.js',
-				options: {
-					ui: 'bdd',
-					reporter: 'spec',
-					require: 'should'
-				}
-			}
 		}
-    });
+	});
 
-    grunt.registerTask('default', ['coffee:compile']);
-    grunt.registerTask('test', ['coffee:test', 'cafemocha:all']);
+	grunt.registerTask('default', ['coffee:compile']);
+	grunt.registerTask('test', ['coffee:test', 'cafemocha:all']);
+
+	grunt.registerTask('publish', [
+		'clean:build',
+		'copy:build',
+		'coffee:compile',
+		'compress:build'
+	]);
 }
